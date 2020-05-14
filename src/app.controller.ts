5c9ace5dbfecde979290d29ca834e6ff176b7fd3
Henning <henning@kuch.email>
2020-05-15 01:25:22 +0200

    Removing view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get, Render } from '@nestjs/common';
 import { KvServiceService } from './kvService/kvService.service';
 import { KvService } from './kvService/kvService.entity';
 
-import * as Handlebars from 'handlebars';
-
 @Controller('notdienste')
 export class AppController {
   constructor(private readonly kvService: KvServiceService) { }
@@ -14,9 +12,8 @@ export class AppController {
   }
 
   @Get('update')
-  // @Render('index')
   async getCurrentServices() {
-    // const services = await this.kvService.getCurrentServices();-
+    // const services = await this.kvService.getCurrentServices();
     const test = KvService.parse(["id", "startDate", "startTime", "endDate", "endTime", "kind", "status", "owner"], "region")
     this.kvService.addService(test);
     return JSON.stringify(test, null, 2)
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 import { AppModule } from './app.module';
-import * as exphbs from 'express-handlebars';
 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-
-  // const helpers = {
-  //   json: (ctx) => JSON.stringify(ctx, null, 2),
-  // };
-  // const hbs = exphbs.create({
-  //   layoutsDir: join(__dirname, '..', 'views'),
-  //   extname: 'hbs',
-  //   defaultLayout: 'index',
-  //   helpers: helpers,
-  // });
-
-  // app.engine('hbs', hbs.engine);
-  // app.setViewEngine('hbs');
 
   let port = process.env.PORT;
   if (port === "" || port === undefined) {
