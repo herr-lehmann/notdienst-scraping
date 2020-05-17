45267e6e0290d80536efa014ebe17f94359fc25d
Henning <henning@kuch.email>
2020-05-18 00:08:50 +0200

    Added handlebar rendering of services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,15 @@ import { Controller, Get, Render } from '@nestjs/common';
 import { KvServiceService } from './kvService/kvService.service';
 import { KvService } from './kvService/kvService.entity';
 
+
 @Controller('notdienste')
 export class AppController {
   constructor(private readonly kvService: KvServiceService) { }
 
   @Get('update')
+  @Render('index')
   async getCurrentServices() {
     const services = await this.kvService.getCurrentServices();
-    return JSON.stringify(services, null, 2)
+    return {services: services}
   }
 }
